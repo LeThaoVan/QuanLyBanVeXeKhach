05a54e6f07c649b76d9ca5022b346e8c182d9a9e
thao van <nyle657@gmail.com>
2022-10-05 14:50:05 +0700

    new

--- src/main/java/com/uav/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.uav.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    
-}
